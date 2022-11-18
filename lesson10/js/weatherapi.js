0f581728281c0db7890454438f35427cdb13d4db
iamvalenciia <val22018@byui.edu>
2022-11-18 13:51:08 -0500

    waiting 401

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -3,7 +3,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&lat=57&lon=-2.15&appid={699d050162036d9d63a2bbc35cc6712f}&units=imperial';
+const requestURL = 'https://api.openweathermap.org/data/2.5/weather?q={Fairbanks}&appid={4d5a5c61370f24e31f31cbdbbdc07ffa}';
 const cards = document.querySelector('.cards');
 
 fetch(requestURL)
@@ -14,3 +14,38 @@ fetch(requestURL)
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     
 });
+
+async function apiFetch() {
+    try {
+      const response = await fetch(requestURL);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        // displayResults(data);
+        displayResults(data)
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+  
+apiFetch();
+
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+}
+
+function  displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
