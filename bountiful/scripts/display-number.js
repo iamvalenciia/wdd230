c53c1bf061266f370d93cfa537aecbb1626e8c8b
iamvalenciia <val22018@byui.edu>
2022-12-17 03:25:30 -0500

    completing about page and last date feature

--- bountiful/scripts/display-number.js
+++ bountiful/scripts/display-number.js
@@ -1,3 +1,11 @@
 // drink count
 let drinkCount = Number(window.localStorage.getItem("drinksMade"));
 document.getElementById('drink-total').textContent = drinkCount;
+
+
+// Last Modified Date
+let lastMod = new Date(document.lastModified);
+let currentCopy = new Date().getFullYear();
+
+let modifiedDate = document.querySelector(".last-modified");
+modifiedDate.innerHTML = lastMod.toLocaleString();
